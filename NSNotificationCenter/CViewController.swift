1829db981a1fc66c0ac5768a7366a0fe048b28ab
Eray Alparslan <erayalparslan@yahoo.com>
2019-12-07 23:33:04 +0300

    initial commit

---
+++ NSNotificationCenter/CViewController.swift
@@ -0,0 +1,26 @@
+//
+//  BViewController.swift
+//  NSNotificationCenter
+//
+//  Created by Eray Alparslan on 7.12.2019.
+//  Copyright © 2019 Eray Alparslan. All rights reserved.
+//
+
+import UIKit
+
+class CViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(checkInternetConnection(_:)),
+                                               name: NSNotification.Name(rawValue: "notificationInternet"), object: nil)
+    }
+    deinit { NotificationCenter.default.removeObserver(self) }
+    
+    @objc func checkInternetConnection(_ notification : Notification) {
+        if let car = notification.object as? Car{
+            print(car.brand)
+            print(car.model)
+        }
+    }
+}
---
+++ NSNotificationCenter/Car.swift
@@ -0,0 +1,18 @@
+//
+//  Car.swift
+//  NSNotificationCenter
+//
+//  Created by Eray Alparslan on 7.12.2019.
+//  Copyright © 2019 Eray Alparslan. All rights reserved.
+//
+
+import Foundation
+
+struct Car{
+    var brand: String
+    var model: String
+    init() {
+        self.brand = "BMW"
+        self.model = "X6"
+    }
+}
